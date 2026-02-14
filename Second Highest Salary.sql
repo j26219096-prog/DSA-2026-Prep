22afdeec0004ecc3bc979728bf5b97ef1ece9bbf
Jawahar R <j26219096@gmail.com>
2026-02-14 14:18:17 +0530

    Implement query to find second highest salary

--- Second Highest Salary.sql
+++ Second Highest Salary.sql
@@ -1,7 +1,9 @@
 SELECT (
-    -- Paste your original query inside here
+  
     SELECT DISTINCT Salary 
     FROM (
         SELECT Salary, DENSE_RANK() OVER (ORDER BY Salary DESC) as rank_num
         FROM Employee
     ) as RankedSalaries
+    WHERE rank_num = 2
+) AS SecondHighestSalary;
