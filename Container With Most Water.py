9ce93f36e1da6bc8df19bb86b1e74f5d8d87c4ff
Jawahar R <j26219096@gmail.com>
2026-02-14 14:05:57 +0530

    Add solution for Container With Most Water problem

---
+++ Container With Most Water.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left,right=0,len(height)-1
+        max_=0
+        while left<right:
+            width=right-left
+            c_length=min(height[left],height[right])
+            max_=max(max_,width*c_length)
