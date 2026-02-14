0b856a0273d1c567cc97bae72e34f863110aedb5
Jawahar R <j26219096@gmail.com>
2026-02-14 14:08:25 +0530

    Implement twoSum function for Two Sum II problem

---
+++ Two Sum II.py
@@ -0,0 +1,7 @@
+class Solution:
+  def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left,right=0,len(numbers)-1
+        while left< right:
+            current=numbers[left]+numbers[right]
+            if current == target:
+                return [left+1,right+1]
