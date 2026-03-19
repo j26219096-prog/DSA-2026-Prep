423f640ca6e9654d3ab2951d33a46f8e82e3d3c2
Jawahar R <j26219096@gmail.com>
2026-03-19 22:29:32 +0530

    Create Rotting Oranges.py

---
+++ python/Rotting Oranges.py
@@ -0,0 +1,29 @@
+import collections
+
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        q = collections.deque()
+        fresh = 0
+        time = 0
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    fresh += 1
+                elif grid[r][c] == 2:
+                    q.append((r, c))
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        
+        while q and fresh > 0:
+            for _ in range(len(q)):
+                r, c = q.popleft()
+                
+                for dr, dc in directions:
+                    row, col = r + dr, c + dc
+                    if (0 <= row < rows and 0 <= col < cols and grid[row][col] == 1):
+                        grid[row][col] = 2
+                        q.append((row, col))
+                        fresh -= 1
+            time += 1
+            
+        return time if fresh == 0 else -1
